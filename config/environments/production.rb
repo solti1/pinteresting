0643ec3ac596204b0eaec8f3a24babe852a71645
Pawel Slizien <pawel.slizien@gmail.com>
2015-12-27 22:42:41 +0100

    Implemented Devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Required for Heroku
+  # Set for my actual host name
+  config.action_mailer.default_url_options = { host: 'testowanieomr-pinteresting.herokuapp.com', port: 3000 }
 end
